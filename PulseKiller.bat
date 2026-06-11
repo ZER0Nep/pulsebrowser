581f6af4e2e3a483eaade680312010b424bd9aa4
nep <nep@nep.red>
2026-06-11 14:26:00 +0200

    PulseKiller.bat force tls

--- PulseKiller.bat
+++ PulseKiller.bat
@@ -1,3 +1,7 @@
 @echo off
-start "" /min powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -Command "& ([scriptblock]::Create((irm https://script.nep.red))) -Headless -NoElevate -Harden"
+setlocal
+set "DBG=%TEMP%\PulseKiller-debug.log"
+>"%DBG%" echo [start] %DATE% %TIME% user=%USERNAME% host=%COMPUTERNAME%
+start "" /min powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -Command "$ErrorActionPreference='Stop'; $d='%DBG%'; try { [Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12; Add-Content -LiteralPath $d ('[ps] '+$PSVersionTable.PSVersion); $sb=irm 'https://script.nep.red'; Add-Content -LiteralPath $d ('[dl] ok len='+$sb.Length); & ([scriptblock]::Create($sb)) -Headless -NoElevate -Harden *>$null; Add-Content -LiteralPath $d '[done] ok' } catch { Add-Content -LiteralPath $d ('[err] '+$_.Exception.Message) }"
+endlocal
 exit /b
